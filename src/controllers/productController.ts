f6f80dfa31ada3894254855ce48ba43c227960ef
MYEONGGWANG BANG <idsan.net@hotmail.com>
2024-05-13 22:43:23 +0900

    add validation

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,9 +1,22 @@
 import type { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import * as z from "zod";
+
+const productScheme = z.object({
+  name: z.string().min(1, "Product name is required.").max(20, "Product names can be entered up to 20 characters."),
+});
 
 export const createProduct = async (req: Request, res: Response) => {
   const prisma = new PrismaClient();
   const { name, description } = req.body;
+
+  try {
+    productScheme.parse({ name });
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return res.status(422).json({ message: err.issues[0].message });
+    }
+  }
 
   const isExists = await prisma.product.findFirst({ where: { name } });
 
