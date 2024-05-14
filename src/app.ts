50cbc4d09cf660a4b6022ecd062767841f0b6716
MYEONGGWANG BANG <idsan.net@hotmail.com>
2024-05-14 21:43:25 +0900

    separate app configuration and server startup

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,8 @@ import express from "express";
 import ProductsRoutes from "./routes/productRoutes";
 
 const app = express();
-const port = 3000;
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 app.use("/products", ProductsRoutes);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+export default app;
